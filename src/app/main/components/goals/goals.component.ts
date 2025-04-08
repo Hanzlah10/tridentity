813342d22c9c7c2a38379e933238e18458815724
Hanzala Sarguroh <hanzalasarguroh@gmail.com>
2025-04-08 14:53:25 +0530

    Add animated progress bar to goals component

--- src/app/main/components/goals/goals.component.ts
+++ src/app/main/components/goals/goals.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-goals',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './goals.component.html',
   styleUrl: './goals.component.css'
 })
 export class GoalsComponent {
-
+  // progress = 15;
 }
