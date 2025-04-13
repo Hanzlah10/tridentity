975313ea5900ab7981857e5faaff1363e004b35f
omjagtap465 <omjagtap465@gmail.com>
2025-04-13 17:55:16 +0530

    Project Resturctred and Implemented Progress Bar

---
+++ src/app/goals/goals.routes.ts
@@ -0,0 +1,8 @@
+import { Route } from '@angular/router';
+import { GoalsComponent } from './goals.component';
+export const goalRoutes: Route[] = [
+  {
+    path: '',
+    component: GoalsComponent,
+  },
+];
